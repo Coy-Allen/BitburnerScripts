efc08a29333fd9f04dddbf36c89ecfddf0e083eb
Coy Allen <callen@ausis.com>
2023-06-13 15:24:45 -0400

    add rainbow and pass sourceFiles to entry point

--- src/cheats/rainbow.ts
+++ src/cheats/rainbow.ts
@@ -21,9 +21,47 @@ function timeCompression(): void {
 */
 
 /*
-no fuckin clue
+brute force
+was given a hint that said the password is lowercase only. NO caps/numbers/symbols/spaces
 */
 
-export function main(_ns: NS) {
-	// TODO: stub
+/* eslint-disable array-element-newline */
+
+const CHARS = [
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+	"k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+	"u", "v", "w", "x", "y", "z",
+];
+/* eslint-enable array-element-newline */
+const MAX = CHARS.length-1;
+const NUM_ARRAY = [-1];
+
+function next(currentCheck: number[]): void {
+	for (let i = 0; i < currentCheck.length; i++) {
+		if (currentCheck[i] === MAX) {
+			currentCheck[i] = 0;
+			continue;
+		}
+		currentCheck[i]++;
+		return;
+	}
+	console.log(`${currentCheck.length} all checked`);
+	currentCheck.push(0);
 }
+function translate(currentCheck: number[]): string {
+	return currentCheck.map(charIndex=>CHARS[charIndex]).join("");
+}
+
+function check(_pass: string): boolean {
+	return false;
+}
+
+export function main(_ns: NS): void {
+	let isCorrect;
+	do {
+		next(NUM_ARRAY);
+		isCorrect = check(translate(NUM_ARRAY));
+	} while (!isCorrect);
+	console.log(`FOUND PASS ${translate(NUM_ARRAY)}`);
+}
+
--- src/generators/part3/runEntrypoint.ts
+++ src/generators/part3/runEntrypoint.ts
@@ -1,11 +1,12 @@
-import {NS} from "@ns";
+import {NS, SourceFileLvl} from "@ns";
 import {getBitnodeMult, getFormulas} from "/generatorResults/part2.js";
 import {remoteMasterBase} from "/lib/remoteMasterBase";
 
-export async function main(ns: NS) {
+export async function main(ns: NS): Promise<void> {
 	ns.disableLog("sleep");
+	const sourceFiles = JSON.parse(ns.read("/generatorResults/part1.txt")) as SourceFileLvl[];
 	const bitnodeMults = getBitnodeMult(ns);
 	const formulas = getFormulas(ns, bitnodeMults);
-	const master = new remoteMasterBase(ns, formulas);
+	const master = new remoteMasterBase(ns, formulas, sourceFiles);
 	await master.run(100);
 }
